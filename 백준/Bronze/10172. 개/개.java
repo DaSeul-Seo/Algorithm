95a9b61a2e617048ceab38b8892ecfc300c81c9b
서다슬 <star.dss999@gmail.com>
2024-01-31 03:36:15 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("|\\_/|\n");
+        bw.write("|q p|   /}\n");
+        bw.write("( 0 )\"\"\"\\\n");
+        bw.write("|\"^\"`    |\n");
+        bw.write("||_/=\\\\__|");
+
+        // 내용 출력
+        bw.flush();
+        // 버퍼 닫기
+        bw.close();
+        br.close();
+    }
+}
