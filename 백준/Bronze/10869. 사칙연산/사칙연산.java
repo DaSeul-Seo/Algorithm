aab7d05356011131b62eaad3485c0db52b190347
서다슬 <star.dss999@gmail.com>
2024-01-31 03:20:12 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        bw.write(String.valueOf(a + b) + "\n");
+        bw.write(String.valueOf(a - b) + "\n");
+        bw.write(String.valueOf(a * b) + "\n");
+        bw.write(String.valueOf(a / b) + "\n");
+        bw.write(String.valueOf(a % b));
+        
+        // 내용 출력
+        bw.flush();
+        // 버퍼 닫기
+        bw.close();
+        br.close();
+    }
+}
