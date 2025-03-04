88601d97c18d93a4b974396e639c79108591f7e0
서다슬 <star.dss999@gmail.com>
2025-03-04 20:11:51 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250304/두 사람/two-person.cpp
+++ 250304/두 사람/two-person.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main() {
     // Please write your code here.
     int age1, age2;
-    string gender1, gender2;
+    char gender1, gender2;
 
     cin >> age1 >> gender1 >> age2 >> gender2;
 
-    if ((age1 >= 19 && gender1 == "M") || (age2 >= 19 || gender2 == "M")) {
+    if ((age1 >= 19 && gender1 == 'M') || (age2 >= 19 || gender2 == 'M')) {
         cout << 1;
     }else {
         cout << 0;
