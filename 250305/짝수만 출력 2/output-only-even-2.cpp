06fc24c029286a34026bb81adc5f73bbe4bc46cd
서다슬 <star.dss999@gmail.com>
2025-03-05 15:25:19 +0900

    Title: 짝수만 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250305/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int b, a;
+    cin >> b >> a;
+
+    int i = b;
+    while(i >= a) {
+        cout << i << " ";
+        i = i - 2;
+    }
+    return 0;
+}
