ad1c919756f597e2bc3a6498b6424c62763b3dc8
서다슬 <star.dss999@gmail.com>
2025-03-05 16:52:55 +0900

    Title: 1까지 나누기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250305/1까지 나누기/divide-by-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int i = 1;
+    int cnt = 1;
+
+    while(n / i != 0) {
+        n = n / i;
+        cnt++;
+        i++;
+    }
+
+    cout << cnt;
+    return 0;
+}
