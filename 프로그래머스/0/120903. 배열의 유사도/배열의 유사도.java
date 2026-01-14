b7b97adf0ebec1b5700143fc55f304c936c4e74d
서다슬 <star.dss999@gmail.com>
2026-01-14 14:23:33 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        
+        Set<String> set = new HashSet<>();
+
+        for (String str : s1) {
+            set.add(str);
+        }
+
+        for (String str : s2) {
+            if (set.contains(str)) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
