6fc83bc03b438ef649b1b7c8a50b1ec1544a4389
서다슬 <star.dss999@gmail.com>
2026-01-14 10:09:00 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 684 ms, Memory: 63824 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,90 @@
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Collections;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        RangeSum();
+    }
+
+    public static void RangeSum() {
+        /*
+            수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오.
+            첫째 줄에 수의 개수 N과 합을 구해야 하는 횟수 M이 주어진다. 
+            둘째 줄에는 N개의 수가 주어진다. 수는 1,000보다 작거나 같은 자연수이다. 
+            셋째 줄부터 M개의 줄에는 합을 구해야 하는 구간 i와 j가 주어진다.
+            총 M개의 줄에 입력으로 주어진 i번째 수부터 j번째 수까지 합을 출력한다.
+            - 1 ≤ N ≤ 100,000
+            - 1 ≤ M ≤ 100,000
+            - 1 ≤ i ≤ j ≤ N
+
+            <예시>
+            5 3
+            5 4 3 2 1
+            1 3
+            2 4
+            5 5
+            ---
+            12
+            9
+            1
+        */
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            StringTokenizer st;
+            StringBuilder sb = new StringBuilder();
+
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int cnt = Integer.parseInt(st.nextToken());
+
+            List<Integer> list = new ArrayList<>();
+            list.add(0);
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= N; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                list.add(list.get(i - 1) + num);
+            }
+
+            for (int i = 0; i < cnt; i++) {
+                st = new StringTokenizer(br.readLine());
+                int start = Integer.parseInt(st.nextToken());
+                int end = Integer.parseInt(st.nextToken());
+
+                int result = list.get(end) - list.get(start - 1);
+                sb.append(result + "\n");
+
+                // for (int j = start; j <= end; j++) {
+                //     result += list.get(j-1);
+                // }
+                // sb.append(result + "\n");
+            }
+
+
+            bw.write(sb.toString());
+            bw.flush();
+            bw.close();
+            br.close();
+        }
+        catch (Exception ex) {
+
+        }
+
+    }
+}
